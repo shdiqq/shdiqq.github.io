979919814847aba4253a65c7690cabd76b1a56c1
shdiqq <71612961+shdiqq@users.noreply.github.com>
2021-07-28 20:10:06 +0700

    Update main.js

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,3 +1,7 @@
+window.addEventListener("load", function(){
+    loader.style.display = "none" ;
+})
+
 alert("The following website is still under development~")
 
 const body = document.querySelector("body");
@@ -9,11 +13,13 @@ const welcome = document.querySelector(".welcome")
 const secondBtn = document.querySelector(".btn-second");
 const center = document.querySelector(".center");
 const selection = document.querySelector(".navbar .menu-list li a");
+const back_section = document.querySelector(".center .fa-chevron-left")
+const article_first = document.querySelector(".article-first");
+const article_second = document.querySelector(".article-second");
+const article_third = document.querySelector(".article-third");
 var loader = document.getElementById("preloader");
 
-window.addEventListener("load", function(){
-    loader.style.display = "none" ;
-})
+
 
 menuBtn.onclick = () => {
     menu.classList.add("active");
@@ -37,10 +43,21 @@ cancelBtn.onclick = () => {
 };
 
 secondBtn.onclick = () => {
-    welcome.classList.add("hide")
-    center.classList.add("show")
+    welcome.classList.add("hide");
+    center.classList.add("show");
+}
+
+back_section.onclick = () => {
+    center.classList.remove("show");
+    welcome.classList.remove("hide");
 }
 
 window.onscroll = () => {
     this.scrollY > 20 ? navbar.classList.add("sticky") : navbar.classList.remove("sticky");
+
+    this.scrollY > 290 ? article_first.classList.add("muncul") : article_first.classList.add("normal");
+
+    this.scrollY > 600 ? article_second.classList.add("muncul") : article_second.classList.add("normal");
+
+    this.scrollY > 900 ? article_third.classList.add("muncul") : article_third.classList.add("normal");
 };
