5289492b3380a15b9e61ba47171f8dea5b0d907e
shdiqq <16520242@mahasiswa.itb.ac.id>
2021-08-08 19:22:36 +0700

    new update

--- asset/js/main.js
+++ asset/js/main.js
@@ -10,13 +10,18 @@ const menu = document.querySelector(".menu-list");
 const menuBtn = document.querySelector(".menu-btn");
 const cancelBtn = document.querySelector(".cancel-btn");
 const welcome = document.querySelector(".welcome")
+const firstBtn = document.querySelector(".btn-first");
+const firstBtnShow = document.querySelector(".if-btn-first .center1");
 const secondBtn = document.querySelector(".btn-second");
-const center = document.querySelector(".center");
+const secondBtnShow = document.querySelector(".if-btn-second .center2");
 const selection = document.querySelector(".navbar .menu-list li a");
-const back_section = document.querySelector(".center .fa-chevron-left")
+const back_section_first = document.querySelector(".if-btn-first .fa-chevron-left")
+const back_section_second = document.querySelector(".if-btn-second .fa-chevron-left")
 const article_first = document.querySelector(".article-first");
 const article_second = document.querySelector(".article-second");
 const article_third = document.querySelector(".article-third");
+const skuy = document.querySelector(".skuyy");
+const masuk = document.querySelector(".masukk");
 var loader = document.getElementById("preloader");
 
 
@@ -42,22 +47,49 @@ cancelBtn.onclick = () => {
     body.classList.remove("disabledScroll");
 };
 
-secondBtn.onclick = () => {
+firstBtn.onclick = () => {
     welcome.classList.add("hide");
-    center.classList.add("show");
+    firstBtnShow.classList.add("show");
+
+    back_section_first.onclick = () => {
+        firstBtnShow.classList.remove("show");
+        welcome.classList.remove("hide");
+    }
 }
 
-back_section.onclick = () => {
-    center.classList.remove("show");
+secondBtn.onclick = () => {
+    welcome.classList.add("hide");
+    secondBtnShow.classList.add("show");
+}
+
+back_section_first.onclick = () => {
+    firstBtnShow.classList.remove("show");
     welcome.classList.remove("hide");
 }
+
+back_section_second.onclick = () => {
+    secondBtnShow.classList.remove("show");
+    welcome.classList.remove("hide");
+}
+
+skuy.onclick = () => {
+    secondBtnShow.classList.remove("show");
+    firstBtnShow.classList.add("show");
+}
+
+masuk.onclick = () => {
+    firstBtnShow.classList.remove("show");
+    secondBtnShow.classList.add("show");
+}
+
+
 
 window.onscroll = () => {
     this.scrollY > 20 ? navbar.classList.add("sticky") : navbar.classList.remove("sticky");
 
-    this.scrollY > 290 ? article_first.classList.add("muncul") : article_first.classList.add("normal");
+    this.scrollY > 400 ? article_first.classList.add("muncul") : article_first.classList.add("normal");
 
-    this.scrollY > 600 ? article_second.classList.add("muncul") : article_second.classList.add("normal");
+    this.scrollY > 700 ? article_second.classList.add("muncul") : article_second.classList.add("normal");
 
-    this.scrollY > 900 ? article_third.classList.add("muncul") : article_third.classList.add("normal");
+    this.scrollY > 1000 ? article_third.classList.add("muncul") : article_third.classList.add("normal");
 };
